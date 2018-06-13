6dd77f85da55ad2a3d258b5682f41297a12449ea
Michael Angerman <stormasm@gmail.com>
2018-06-13 08:19:12 -0700

    v2 initial commit

---
+++ v2/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+//import App from './simpletabs';
+//import App from './MenuAppBarOrig';
+//import App from './MenuAppBar';
+import App from './MenuAppBar3';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
