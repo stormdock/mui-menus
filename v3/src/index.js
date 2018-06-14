cc271191b876e112292bedcf113959aeffca577a
Michael Angerman <stormasm@gmail.com>
2018-06-14 13:02:36 -0700

    MenuAppBar2

--- v3/src/index.js
+++ v3/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 //import Demo from './SimpleMenu';
-import Demo from './MenuAppBar';
+import Demo from './MenuAppBar2';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
