cc88b8652e1d1700521a0e5970ff13c7b2054d61
Michael Angerman <stormasm@gmail.com>
2018-06-21 11:04:31 -0700

    MenuAppBar3

--- v4/src/index.js
+++ v4/src/index.js
@@ -1,10 +1,9 @@
-
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 //import Demo from './SimpleMenu';
-import Demo from './MenuAppBar2';
+import Demo from "./MenuAppBar3";
 
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector("#root");
 if (rootElement) {
   render(<Demo />, rootElement);
 }
